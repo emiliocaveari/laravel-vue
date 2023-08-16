09440594c74aae57688f42f49a705cf9bf2150cf
Emilio C Marques <emiliocaveari@gmail.com>
2023-08-16 19:17:22 -0300

    Rename title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,13 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>How To Install Vue 3 in Laravel 9 with Vite</title>
+	<title>Laravel and Vue</title>
 
 	@vite('resources/css/app.css')
 </head>
 <body>
 	<div id="app"></div>
-
 	@vite('resources/js/app.js')
 </body>
 </html>
